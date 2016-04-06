10aad794bb6dc3ea2f0b2a4ef7e7b35df5a29ea8
Gevfg <geversongislon@hotmail.com>
2016-04-06 14:18:27 -0300

    Alteração no login
    Retirando os sistemas de login via modal

--- header.php
+++ header.php
@@ -1,7 +1,4 @@
 <!DOCTYPE html>
-<?php
-$nome = "login";
-?>
 
 <html lang="pt-br">
   <head>
@@ -27,7 +24,7 @@ $nome = "login";
         <div class="container">
             <div class="row">
                 <div class="col-xs-2 text-left">
-                    <a class="user" href="" data-toggle="modal" data-target=".bs-example-modal-sm"><i class="fa fa-user"></i ><span class="oculto"> <?php echo $nome ?></span></a>
+                    <a class="user" href="" data-toggle="modal" data-target=".bs-example-modal-sm"><i class="fa fa-user"></i ></a>
                 </div>
                 <div class="col-xs-10">
                     <ul class="list-inline text-right">
@@ -89,40 +86,4 @@ $nome = "login";
     </div>
 
 
-<!-- Modal -->
-    <div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-        <div class="modal-dialog modal-sm">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="myModalLabel">Minha Conta</h4>
-                </div>
-                <div class="modal-body">
-                    <ul class="nav nav-tabs" role="tablist">
-                        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">logar</a></li>
-                        <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Cadastrar</a></li>
-                    </ul>
-                    <!-- Tab panes -->
-                    <div class="tab-content">
-                        <div role="tabpanel" class="tab-pane active" id="home">
-                            <form style="margin-top:10px;" action="login.php" method="post">
-                                <div class="form-group">
-                                    <label class="logar" for="exampleInputEmail1">Email </label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" name="nome" placeholder="nome">
-                                </div>
-                                <div class="form-group">
-                                    <label class="logar" for="exampleInputPassword1">Senha</label>
-                                    <input type="password" name="senha" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                                </div>
-                                <button type="submit" class="btn btn-logar">Logar</button>
-                            </form>
-                        </div>
-                        <div role="tabpanel" class="tab-pane" id="profile">
-                        <p>Aguardando...</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-      </div>
       
---
+++ login.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PORTFÓLIO GEVERSON</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <div class="container">
+  	<div class="col-sm-4 col-sm-offset-4">
+      <form class="form-signin">
+        <h3 class="form-signin-heading">LOGAR</h3>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="text" id="inputEmail" class="form-control" placeholder="Nome" required autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="senha" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">LOGAR</button>
+      </form>
+     </div>
+    </div> <!-- /container -->
+
+
+
+
+
+  	<script src="js/bootstrap.min.js"></script>
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+  </body>
